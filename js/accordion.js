19f165be14027ce0a9f87f180f50c26d31edbfe2
LV <viveiros.lucas@hotmail.com>
2023-03-02 12:11:04 +0000

    add supervisors

--- js/accordion.js
+++ js/accordion.js
@@ -1,4 +1,5 @@
-$(document).ready(function () {
-    MDB.accordion.init();
+$(document).ready(function() {
+  $("#headingOne").click(function() {
+    $(this).find("i").toggleClass("fa-angle-down fa-angle-up");
   });
-  
+});
